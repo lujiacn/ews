07d600a054e75b5d7e640243881696a73e043baa
Jia Lu <lujiacn@gmail.com>
2019-01-24 21:51:29 +0800

    change html

--- createitem.go
+++ createitem.go
@@ -8,47 +8,47 @@ import (
 // https://msdn.microsoft.com/en-us/library/office/aa563009(v=exchg.140).aspx
 
 type CreateItem struct {
-	XMLName		struct{}	`xml:"m:CreateItem"`
-	MessageDisposition		string	`xml:"MessageDisposition,attr"`
-	SavedItemFolderId	SavedItemFolderId	`xml:"m:SavedItemFolderId"`
-	Items			Messages		`xml:"m:Items"`
+	XMLName            struct{}          `xml:"m:CreateItem"`
+	MessageDisposition string            `xml:"MessageDisposition,attr"`
+	SavedItemFolderId  SavedItemFolderId `xml:"m:SavedItemFolderId"`
+	Items              Messages          `xml:"m:Items"`
 }
 
 type Messages struct {
-	Message		[]Message		`xml:"t:Message"`
+	Message []Message `xml:"t:Message"`
 }
 
 type SavedItemFolderId struct {
-	DistinguishedFolderId	DistinguishedFolderId	`xml:"t:DistinguishedFolderId"`
+	DistinguishedFolderId DistinguishedFolderId `xml:"t:DistinguishedFolderId"`
 }
 
 type DistinguishedFolderId struct {
-	Id		string		`xml:"Id,attr"`
+	Id string `xml:"Id,attr"`
 }
 
 type Message struct {
-	ItemClass		string		`xml:"t:ItemClass"`
-	Subject		string		`xml:"t:Subject"`
-	Body			Body			`xml:"t:Body"`
-	Sender		OneMailbox	`xml:"t:Sender"`
-	ToRecipients	XMailbox		`xml:"t:ToRecipients"`
+	ItemClass    string     `xml:"t:ItemClass"`
+	Subject      string     `xml:"t:Subject"`
+	Body         Body       `xml:"t:Body"`
+	Sender       OneMailbox `xml:"t:Sender"`
+	ToRecipients XMailbox   `xml:"t:ToRecipients"`
 }
 
 type Body struct {
-	BodyType		string	`xml:"BodyType,attr"`
-	Body			[]byte	`xml:",chardata"`
+	BodyType string `xml:"BodyType,attr"`
+	Body     []byte `xml:",chardata"`
 }
 
 type OneMailbox struct {
-	Mailbox		Mailbox			`xml:"t:Mailbox"`
+	Mailbox Mailbox `xml:"t:Mailbox"`
 }
 
 type XMailbox struct {
-	Mailbox		[]Mailbox			`xml:"t:Mailbox"`
+	Mailbox []Mailbox `xml:"t:Mailbox"`
 }
 
 type Mailbox struct {
-	EmailAddress		string		`xml:"t:EmailAddress"`
+	EmailAddress string `xml:"t:EmailAddress"`
 }
 
 func BuildTextEmail(from string, to []string, subject string, body []byte) ([]byte, error) {
@@ -58,7 +58,7 @@ func BuildTextEmail(from string, to []string, subject string, body []byte) ([]by
 	m := new(Message)
 	m.ItemClass = "IPM.Note"
 	m.Subject = subject
-	m.Body.BodyType = "Text"
+	m.Body.BodyType = "HTML"
 	m.Body.Body = body
 	m.Sender.Mailbox.EmailAddress = from
 	mb := make([]Mailbox, len(to))
